2807cbac1e797422669033a38d37e2e6ecf71669
paulrowe10 <paul.rowe@mcvts.org>
2024-09-10 09:16:24 -0400

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name = "Paul";
+    int age = 15;
+    float time = 9.09;
+    bool answer = true;
+    char inital = 'p';
+
+    cout << name << " " << age << " " << time << " " << answer << " " << inital << " "<< endl;
+}
